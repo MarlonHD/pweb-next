7d5e6501216615200f513b5ad794e0efddc87b4b
MarlonHD <marlonsilva840@gmail.com>
2023-11-15 20:19:19 -0300

    Receita 1

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,28 @@ export default function Home(){
     return (
         <div>
             <h2>
-                Viva Santana!
+                Esta é a página inicial.
             </h2>
         </div>
     ) 
 }
+
+export function Teste(){
+
+    return(
+        <div>
+            <h1>Função criada em index.js</h1>
+        </div>
+    )
+}
+
+export function Titulo(){
+    return(
+        <div>
+            <h2>
+                SONETO DE FIDELIDADE
+            </h2>
+        </div>
+    )
+}
+
---
+++ pages/receita01/pagina1.js
@@ -0,0 +1,12 @@
+import { Teste } from "..";
+
+export default function pagina1(){
+    return(
+        <div>
+            <h2>
+                Esta é a página 1
+            </h2>
+            <Teste/>
+        </div>
+    )
+}
